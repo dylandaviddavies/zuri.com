6fd180b40559745d3ab742c83b1e940a5d3f844b
Dylan Davies <dylandaviddavies@gmail.com>
2020-06-21 03:02:46 -0400

    'deploy'

---
+++ precache-manifest.5f346af8af2eab5bbb5b850035820f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec636f56a6ebf9c6df9241744eedb016",
+    "url": "/zuri.com/index.html"
+  },
+  {
+    "revision": "478c5fff2e6cf21493eb",
+    "url": "/zuri.com/static/css/main.32ef8ddd.chunk.css"
+  },
+  {
+    "revision": "25a903715b3712305060",
+    "url": "/zuri.com/static/js/2.0e54157c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/zuri.com/static/js/2.0e54157c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478c5fff2e6cf21493eb",
+    "url": "/zuri.com/static/js/main.1942002a.chunk.js"
+  },
+  {
+    "revision": "300e34b655741b2abe59",
+    "url": "/zuri.com/static/js/runtime-main.987b2ce2.js"
+  },
+  {
+    "revision": "605f7429634fa51ddf49acbae38c1b61",
+    "url": "/zuri.com/static/media/logo.605f7429.svg"
+  }
+]);
